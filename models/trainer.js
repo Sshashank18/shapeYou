f0d343da59ab7072a04d7714c0e053906f6d5e42
Arnav Jain <arrnavjain@gmail.com>
2020-09-26 22:48:24 +0530

    added first trainer model

---
+++ models/trainer.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var trainerSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    contact: Number,
+    dob: Date,
+    startDate: Date,
+    endDate: Date,
+    user_id: String
+});
+
+module.exports = mongoose.model("Trainer", trainerSchema);

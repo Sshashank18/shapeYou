e493043fdc074d3088035354cf3aff32417bf20a
Arnav Jain <arrnavjain@gmail.com>
2020-11-25 17:14:09 +0530

    changes

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,93 +1,87 @@
 function MouseWheelHandler(e, element) {
-    var delta = 0;
-    if (typeof e === 'number') {
-      delta = e;
+  var delta = 0;
+  if (typeof e === 'number') {
+    delta = e;
+  } else {
+    if (e.deltaX !== 0) {
+      delta = e.deltaX;
     } else {
-      if (e.deltaX !== 0) {
-        delta = e.deltaX;
-      } else {
-        delta = e.deltaY;
-      }
-      e.preventDefault();
+      delta = e.deltaY;
     }
-  
-    element.scrollLeft -= (delta);
-  
+    e.preventDefault();
   }
+
+  element.scrollLeft -= (delta);
+
+}
+
+window.onload = function() {
+  var carousel = {};
+  carousel.e = document.getElementById('carousel');
+  carousel.items = document.getElementById('carousel-items');
+  carousel.leftScroll = document.getElementById('left-scroll-button');
+  carousel.rightScroll = document.getElementById('right-scroll-button');
+
+  carousel.items.addEventListener("mousewheel", handleMouse, false);
+  carousel.items.addEventListener("scroll", scrollEvent);
+  carousel.leftScroll.addEventListener("click", leftScrollClick);
+  carousel.rightScroll.addEventListener("click", rightScrollClick);
+ /* carousel.leftScroll.addEventListener("mousedown", leftScrollClick);
+  carousel.rightScroll.addEventListener("mousedown", rightScrollClick);*/
+
+  setLeftScrollOpacity();
+  setRightScrollOpacity();
   
-  window.onload = function() {
+  function handleMouse(e) {
+    MouseWheelHandler(e, carousel.items);
+  }
 
-    var len = document.getElementsByClassName('carouselBox').length;
+  function leftScrollClick() {
+    MouseWheelHandler(100, carousel.items);
+  }
 
-    for(var i=1;i<=len;i++){
+  function rightScrollClick() {
+    MouseWheelHandler(-100, carousel.items);
+  }
 
-    var carousel = {};
-    carousel.e = document.getElementById('carousel'+i);
-    carousel.items = document.getElementById('carousel-items'+i);
-    carousel.leftScroll = document.getElementById('left-scroll-button'+i);
-    carousel.rightScroll = document.getElementById('right-scroll-button'+i);
-
-    carousel.items.addEventListener("mousewheel", handleMouse, false);
-    carousel.items.addEventListener("scroll", scrollEvent);
-    carousel.leftScroll.addEventListener("click", leftScrollClick);
-    carousel.rightScroll.addEventListener("click", rightScrollClick);
-   /* carousel.leftScroll.addEventListener("mousedown", leftScrollClick);
-    carousel.rightScroll.addEventListener("mousedown", rightScrollClick);*/
-  
+  function scrollEvent(e) {
     setLeftScrollOpacity();
     setRightScrollOpacity();
-    
-    function handleMouse(e) {
-      MouseWheelHandler(e, carousel.items);
-    }
+  }
   
-    function leftScrollClick() {
-      MouseWheelHandler(100, carousel.items);
+  function setLeftScrollOpacity() {
+    if (isScrolledAllLeft()) {
+      carousel.leftScroll.style.opacity = 0;
+    } else {
+      carousel.leftScroll.style.opacity = 1;
     }
+  }
   
-    function rightScrollClick() {
-      MouseWheelHandler(-100, carousel.items);
-    }
-  
-    function scrollEvent(e) {
-      setLeftScrollOpacity();
-      setRightScrollOpacity();
-    }
-    
-    function setLeftScrollOpacity() {
-      if (isScrolledAllLeft()) {
-        carousel.leftScroll.style.opacity = 0;
-      } else {
-        carousel.leftScroll.style.opacity = 1;
-      }
-    }
-    
-    function isScrolledAllLeft() {
-      if (carousel.items.scrollLeft === 0) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    
-    function isScrolledAllRight() {
-      if (carousel.items.scrollWidth > carousel.items.offsetWidth) {
-        if (carousel.items.scrollLeft + carousel.items.offsetWidth === carousel.items.scrollWidth) {
-          return true;
-        } 
-      }else {
-        return true;
-      }
-      
+  function isScrolledAllLeft() {
+    if (carousel.items.scrollLeft === 0) {
+      return true;
+    } else {
       return false;
     }
-    
-    function setRightScrollOpacity() {
-      if (isScrolledAllRight()){
-          carousel.rightScroll.style.opacity = 0;
-        } else {
-          carousel.rightScroll.style.opacity = 1;
-        }
+  }
+  
+  function isScrolledAllRight() {
+    if (carousel.items.scrollWidth > carousel.items.offsetWidth) {
+      if (carousel.items.scrollLeft + carousel.items.offsetWidth === carousel.items.scrollWidth) {
+        return true;
+      } 
+    }else {
+      return true;
     }
+    
+    return false;
+  }
+  
+  function setRightScrollOpacity() {
+    if (isScrolledAllRight()){
+        carousel.rightScroll.style.opacity = 0;
+      } else {
+        carousel.rightScroll.style.opacity = 1;
+      }
   }
 }

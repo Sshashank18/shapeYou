7831459e44ded30e413d5d16349e785159816092
Shashank Aggarwal <shashankaggarwal13@gmail.com>
2020-12-30 01:20:04 +0530

    site updated

--- routes/user.js
+++ routes/user.js
@@ -63,8 +63,8 @@ route.post('/paytm',middleware.isUserLoggedIn,(req,res)=>{
             "TXN_AMOUNT" : price,
             // "CALLBACK_URL" :`${DOMAIN}success?name=${req.query.name}&email=${req.query.email}&mobile=${req.query.mobile}&branch=${req.query.branch}&year=${req.query.year}&college=${req.query.college}&event=${req.query.event}&amount=${req.query.amount}`,
             // "CALLBACK_URL" :`http://127.0.0.1:3500/user/newSession?body=${encodeURIComponent( JSON.stringify(req.body) )}&id=${req.user._id}`,
-            // "CALLBACK_URL" :`https://shapeyou-demo.herokuapp.com/user/newSession?body=${encodeURIComponent( JSON.stringify(req.body) )}&id=${req.user._id}`,
-            "CALLBACK_URL" :`http://127.0.0.1:3500/user/newSession?body=${encodeURIComponent( JSON.stringify(req.body) )}&id=${req.user._id}`,
+            "CALLBACK_URL" :`https://shapeyou-demo.herokuapp.com/user/newSession?body=${encodeURIComponent( JSON.stringify(req.body) )}&id=${req.user._id}`,
+            // "CALLBACK_URL" :`http://127.0.0.1:3500/user/newSession?body=${encodeURIComponent( JSON.stringify(req.body) )}&id=${req.user._id}`,
         };
         
         checksum_lib.genchecksum(paytmParams, "u#R7ezMHf4rNiJ3J", function(err, checksum){

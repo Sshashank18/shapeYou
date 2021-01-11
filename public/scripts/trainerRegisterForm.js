88604e39c2abe92433791ed6df6b1770926aa5d9
Arnav Jain <arrnavjain@gmail.com>
2021-01-11 21:13:47 +0530

    form validations

--- public/scripts/trainerRegisterForm.js
+++ public/scripts/trainerRegisterForm.js
@@ -91,6 +91,21 @@ $('#uploadResume').click(e=>{
     });
 });
 
+$(function(){
+    var dtToday = new Date();
+    
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear();
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+    
+    var maxDate = year + '-' + month + '-' + day;
+    $('#dob').attr('max', maxDate);
+});
+
 
 
 $('#formSubmit').click(e => {
